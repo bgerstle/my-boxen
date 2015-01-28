5eb43138f2c913a49a073d11fed294671b4358c0
Brian Gerstle <bgerstle@Administrators-MacBook-Pro.local>
2015-01-28 14:59:21 -0800

    fixed lots of stuff
    - changed global gitignore in yaml
    - changed our-boxen => my-boxen
    - fixed pyenv-virtualenvwrapper by adding env script and fixing usage
    - fixed intellij (now doing it manually)
    - remove accidentally added .swp files?
    - some general refactor

--- modules/people/manifests/btgerst/apps.pp
+++ modules/people/manifests/btgerst/apps.pp
@@ -2,7 +2,9 @@ class people::btgerst::apps {
   include macvim
   include iterm2::stable
 
-  class { 'intellij':
-    edition => 'community',
+  package { 'Intellij-IDEA-IC-14.0.3':
+    ensure   => 'present',
+    provider => 'appdmg_eula',
+    source   => 'http://download.jetbrains.com/idea/ideaIC-14.0.3-jdk-bundled.dmg'
   }
 }
