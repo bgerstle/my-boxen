bb302d5093432d6f428c82e41f90e3642827ce06
Brian Gerstle <brian.gerstle@gmail.com>
2015-01-28 15:38:17 -0800

    moved lang global versions into yaml, tweaked site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -65,16 +65,11 @@ node default {
     fail('Please enable full disk encryption and try again')
   }
 
-  # node versions
-  nodejs::version { 'v0.6': }
-  nodejs::version { 'v0.8': }
-  nodejs::version { 'v0.10': }
+  # version defined in hiera
+  include nodejs::global
 
   # default ruby versions
   ruby::version { '1.9.3': }
-  ruby::version { '2.0.0': }
-  ruby::version { '2.1.0': }
-  ruby::version { '2.1.1': }
   ruby::version { '2.1.2': }
 
   # common, useful packages
@@ -82,7 +77,8 @@ node default {
     [
       'ack',
       'findutils',
-      'gnu-tar'
+      'gnu-tar',
+      'autoconf'
     ]:
   }
 
