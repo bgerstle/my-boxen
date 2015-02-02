7807ea8f88417a914e3c248ac47e1b070aa70944
Brian Gerstle <brian.gerstle@gmail.com>
2015-02-02 11:50:17 -0500

    add OSX keyboard defaults

--- modules/people/manifests/btgerst/prefs.pp
+++ modules/people/manifests/btgerst/prefs.pp
@@ -1,21 +1,29 @@
+# OSX System & App preferences
 class people::btgerst::prefs {
   require stdlib
+  require people::btgerst::dotfiles
 
-  # define a resource which will ensure & install a plist preference file
-  # and then (optionally) restart the app
+  # OSX settings
+  include osx::global::key_repeat_rate
+  include osx::global::key_repeat_delay
+  include osx::global::disable_key_press_and_hold
+  include osx::finder::show_hidden_files
+
+  # Individual app preferences
+  # TODO: move into dotfiles?
   define app_preferences(
     $prefs_domain = undef,
     $killall = undef) {
     $abs_filepath = "${boxen::config::srcdir}/dotfiles/${title}"
     file { $abs_filepath:
-      ensure => present,
+      ensure  => present,
       require => Repository["${boxen::config::srcdir}/dotfiles"]
     }
 
     $install_cmd = "defaults import ${prefs_domain} ${title}"
     exec { $install_cmd:
       cwd         => "${boxen::config::srcdir}/dotfiles",
-      subscribe   => File["${abs_filepath}"],
+      subscribe   => File[$abs_filepath],
       require     => Repository["${boxen::config::srcdir}/dotfiles"],
       refreshonly => true
     }
