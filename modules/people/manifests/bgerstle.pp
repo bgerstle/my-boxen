bc52caa664e8a7ef0540f45b7456c5ff6044fcc9
Brian Gerstle <brian.gerstle@gmail.com>
2015-08-02 13:49:36 -0400

    bump puppet-git & -homebrew

--- modules/people/manifests/bgerstle.pp
+++ modules/people/manifests/bgerstle.pp
@@ -1,33 +1,33 @@
 # module for Brian Gerstle
 class people::bgerstle {
-  include shell
-  include apps
-  include dotfiles
-  include prefs
-  include tools
-  include python
-  include java
-  include haskell
-  include git
+  include people::bgerstle::shell
+  include people::bgerstle::apps
+  include people::bgerstle::dotfiles
+  include people::bgerstle::prefs
+  include people::bgerstle::tools
+  include people::bgerstle::python
+  include people::bgerstle::java
+  include people::bgerstle::haskell
+  include people::bgerstle::git
 
   $oh_my_zsh_srcdir = "${boxen::config::srcdir}/oh-my-zsh"
-  repository { "${oh_my_zsh_srcdir}":
+  repository { $oh_my_zsh_srcdir:
     source => 'robbyrussell/oh-my-zsh',
   }
-  file { "${my_home}/.oh-my-zsh":
+  file { "${::my_home}/.oh-my-zsh":
     ensure  => link,
     target  => $oh_my_zsh_srcdir,
     require => Repository[$oh_my_zsh_srcdir]
   }
 
   $env_vars_to_export = ['PATH',
-                         'JAVA_HOME',
-                         'M2_HOME',
-                         'RUBY_HOME',
-                         'PYENV_HOME',
-                         'NODENV_ROOT',
-                         'BUNDLE_BIN_PATH',
-                         'GEM_HOME']
+                        'JAVA_HOME',
+                        'M2_HOME',
+                        'RUBY_HOME',
+                        'PYENV_HOME',
+                        'NODENV_ROOT',
+                        'BUNDLE_BIN_PATH',
+                        'GEM_HOME']
 
   $joined_env_vars = join($env_vars_to_export, ' ')
 
