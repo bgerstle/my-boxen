628728dfe662e9fee65e63eb5f675971fd66a80f
Brian Gerstle <brian.gerstle@gmail.com>
2016-04-02 12:50:49 -0400

    add local node_modules/.bin to PATH

--- modules/people/manifests/bgerstle/shell.pp
+++ modules/people/manifests/bgerstle/shell.pp
@@ -7,6 +7,10 @@ class people::bgerstle::shell {
   # See def in hiera
   include nodejs::global
 
+  boxen::env_script { 'local_node_bin_on_path':
+    content => 'export PATH=node_modules/.bin:$PATH'
+  }
+
   $oh_my_zsh_dir = "/Users/${::boxen_user}/.oh-my-zsh"
   repository { $oh_my_zsh_dir:
     source => 'robbyrussell/oh-my-zsh',
